d2f1c77dee6541c1398ccf4640a3da739795d7d4
arf1e <anotheruselessbox@gmail.com>
2021-10-12 15:11:55 +0300

    refactor

--- src/cli.js
+++ src/cli.js
@@ -7,26 +7,4 @@ const greet = () => {
   return username;
 };
 
-export const even = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const username = greet();
-  let wins = 0;
-  while (wins < 3) {
-    const number = Math.ceil(Math.random() * 20);
-    console.log(`Question: ${number}`);
-    const guess = readLineSync.question('Your answer: ');
-    const isEven = number % 2 === 0;
-    const correctAnswer = isEven ? 'yes' : 'no';
-    if (guess === correctAnswer) {
-      wins += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`${guess} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${username}!`);
-      wins = 0;
-    }
-  }
-  console.log(`Congratulations, ${username}!`);
-};
-
 export default greet;
